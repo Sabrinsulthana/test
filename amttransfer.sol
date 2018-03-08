a881002c1e187b045e638b8ad473c7a72cf40e41
Sabrinsulthana <31950003+Sabrinsulthana@users.noreply.github.com>
2018-03-08 15:59:31 +0530

    Add files via upload

--- amttransfer.sol
+++ amttransfer.sol
@@ -5,26 +5,26 @@ contract amttransfer
     string public  name="sabrin";
     string  public symbol="$";
     uint public totalSupply=10000;
+    
 
     mapping(address=>uint)public balances;
     
-    function transfer(address to,uint value)public view returns(bool)
+    function transfer(address to,uint value)public
     { 
         balances[msg.sender]=balances[msg.sender]-(value);
         balances[to] =balances[to]+(value);
-        return(true);
+    
     }
       function mint(uint amount) public  view returns (bool)
         {
          totalSupply = totalSupply+amount;
-         if(amount>0)
-         {
-         return (true);
-        } 
+         require(amount<=totalSupply);
+         if(amount<0){
+             return(true);
+        }
         else
         {
             return(false);
         }
         }
-        
 }
